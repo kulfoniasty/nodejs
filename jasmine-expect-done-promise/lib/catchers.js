cc7a482882630d2faf9d1ac532e7dbc0cb005608
gm <grzegorz@musial.tk>
2014-11-15 14:57:04 +0100

    Improved catchers with proper stacktraces

--- jasmine-expect-done-promise/lib/catchers.js
+++ jasmine-expect-done-promise/lib/catchers.js
@@ -1,7 +1,8 @@
 'use strict';
 
 exports.catcher = function(done) {
+  var e = new Error();
   return function(err){
-    done(err);
+    done(err.stack !== undefined ? err : e);
   };
 };
--- jasmine-expect-done-promise/spec/testSpec.js
+++ jasmine-expect-done-promise/spec/testSpec.js
@@ -7,7 +7,7 @@ const
 
 describe('GIVEN resolved (positive) promise ', function () {
 
-   it("WHEN then is called and exception inside THEN exception is thrown", function (done) {
+   it("WHEN then is called and exception inside THEN exception is thrown and stacktrace is taken from the exception", function (done) {
       promiseService.resolvedPromise()
         .then(function (success) {
           expect("something to show expectations failing along with exception").toBe(null);
@@ -15,5 +15,13 @@ describe('GIVEN resolved (positive) promise ', function () {
         })
         .catch(catchers.catcher(done));
     });
+});
 
+
+describe('GIVEN rejected promise ', function () {
+
+   it("WHEN rejected and get to catch THEN exception is has a stacktrace leading to .catch() statement line", function (done) {
+      promiseService.rejectedPromise()
+        .catch(catchers.catcher(done));
+    });
 });
